b1f5e331fd9fd156356996c2546679892e3a5b3d
uke <ax.remack@gmail.com>
2022-02-02 11:10:27 +0100

    :sparkles: Adding traduction and hints (but can't print winning message...)

--- TP2_springboot (formulaires)/src/main/java/app/Devinette.java
+++ TP2_springboot (formulaires)/src/main/java/app/Devinette.java
@@ -6,22 +6,23 @@ import java.util.Random;
 
 
 public class Devinette {
-    String message;
-    int nombre;
+    //String message;
+    public int nombre;
+    public boolean isFound;
 
-    @Range(min = 1, max = 100)
-    int proposition;
+    @Range(min = 0, max = 100)
+    public int proposition;
 
     static Random random = new Random();
 
     public Devinette() {
         nombre  = 1 + random.nextInt(100);
-        message = "Je pense à un nombre entre 1 et 100 ("+nombre+"). Pouvez-vous le trouver ?";
+        //message = "Je pense à un nombre entre 1 et 100 ("+nombre+"). Pouvez-vous le trouver ?";
     }
 
-    public String getMessage() {
+    /*public String getMessage() {
         return message;
-    }
+    }*/
 
     public int getProposition() {
         return proposition;
@@ -37,6 +38,6 @@ public class Devinette {
 
     public void reset() {
         this.nombre  = 1 + this.random.nextInt(100);
-        this.message = "Je pense à un nombre entre 1 et 100 ("+this.nombre+"). Pouvez-vous le trouver ?";
+        //this.message = "Je pense à un nombre entre 1 et 100 ("+this.nombre+"). Pouvez-vous le trouver ?";
     }
 }
